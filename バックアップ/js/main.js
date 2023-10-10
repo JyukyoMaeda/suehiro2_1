934ebfc4f9f9a6176d0f0fff7ec4813fc8ee3512
JyukyoMaeda <maeda-r@jyukyo.com>
2023-10-10 18:12:37 +0900

    タブコンテンツ > 内観・外観 > 反映

---
+++ バックアップ/js/main.js
@@ -0,0 +1,22 @@
+// let elem1Data = {
+//     'domObj':$('#content4'),
+//     'elemMovePos':0,
+//     'offY':$('#content4').offset().top,
+//     'offX':$('#content4').offset().left
+// //   },
+//     };
+
+// elem1Data.domObj.css('background-position','left -200px');
+// let y = 0;
+// let y2 = 1;
+
+// $(window).on("scroll",function(){
+//     y = $(this).scrollTop();
+//     if(y > elem1Data.elemMovePos)
+//     {
+//         elem1 = ((parseInt(y)-parseInt(elem1Data.elemMovePos))*1.1)-200;
+//         elem1Data.domObj.css('background-position','left ' + elem1 +'px');
+//     }else{
+//     elem1Data.domObj.css('background-position','left -200px');
+//     }
+// });
